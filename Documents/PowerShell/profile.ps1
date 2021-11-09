5fb5d492de7bb5d405fb8d762660d06f11e00f4b
Tinson Lai <laitingsheng@hotmail.com>
2021-11-09 20:31:49 +0800

    Add PowerShell profiles

---
+++ Documents/PowerShell/profile.ps1
@@ -0,0 +1,6 @@
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\laitingsheng\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
+Set-PoshPrompt -Theme powerlevel10k_modern
