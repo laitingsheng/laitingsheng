00b7ff2ac0575119726180d9350a85b47cec2690
Tinson Lai <laitingsheng@hotmail.com>
2022-03-01 11:38:06 +0800

    Customise OhMyPosh theme based on powerlevel10k_classic/modern

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -3,4 +3,5 @@
 (& "$Env:USERPROFILE\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
 #endregion
 
-Set-PoshPrompt -Theme powerlevel10k_modern
+Set-PoshPrompt -Theme ~/.powershell.omp.json
+Enable-PoshTransientPrompt
